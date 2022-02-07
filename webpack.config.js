2450d1ddb70e9726746fed45249b65dc43b8e7a7
Oleg Cherr <olegcherr@gmail.com>
2022-02-07 14:15:52 +0400

    Enable cleaning output dir

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,7 @@ module.exports = [
     output: {
       filename: 'server.js',
       path: path.resolve(__dirname, 'dist'),
+      clean: true,
     },
     plugins: [new ServerStartPlugin()],
   },
