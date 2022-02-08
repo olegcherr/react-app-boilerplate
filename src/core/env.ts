ff321e5bd88f3e294e25b1a0ab86d20377640fba
Oleg Cherr <olegcherr@gmail.com>
2022-02-08 12:51:38 +0400

    Add 'isRequired' param to 'env'

--- src/core/env.ts
+++ src/core/env.ts
@@ -1,11 +1,11 @@
 import { invariant } from './invariant'
 import { isBrowser } from './misc'
 
-export function env(name: string) {
+export function env(name: string, isRequired = true) {
   // @ts-ignore __ENV is created by 'react-env'
   const value = isBrowser ? window.__ENV[name] : process.env[name]
 
-  invariant(value, `ENV variable "${name}" is not defined`)
+  invariant(!isRequired || value, `ENV variable "${name}" is not defined`)
 
   return value
 }
