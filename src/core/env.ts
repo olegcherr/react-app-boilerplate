18c3b6d76b423fad726d14d07fabe121383a99af
Oleg Cherr <olegcherr@gmail.com>
2022-02-22 12:32:01 +0400

    Add generic type to 'env'

--- src/core/env.ts
+++ src/core/env.ts
@@ -1,11 +1,11 @@
 import { invariant } from './invariant'
 import { isBrowser } from './misc'
 
-export function env(name: string, isRequired = true) {
+export function env<T = string>(name: string, isRequired = true): T {
   // @ts-ignore __ENV is created by 'react-env'
   const value = isBrowser ? window.__ENV[name] : process.env[name]
 
   invariant(!isRequired || value, `ENV variable "${name}" is not defined`)
 
-  return value
+  return value as unknown as T
 }
